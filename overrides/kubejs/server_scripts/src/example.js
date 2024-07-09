1fad4c158dc8f94cc2eec46aa690eaf0c7df0d2a
YoungOnion <39562198+YoungOnionMC@users.noreply.github.com>
2024-07-09 16:46:29 -0600

    initial commit

---
+++ overrides/kubejs/server_scripts/src/example.js
@@ -0,0 +1,17 @@
+// priority: 0
+
+// Visit the wiki for more info - https://kubejs.com/
+
+console.info('Hello, World! (Loaded server scripts)')
+
+ServerEvents.recipes(event => {
+
+    
+    
+
+
+})
+
+GTCEu.recipes(event => {
+    
+})
